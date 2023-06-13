637966d90e668a901cdb6882106b843fa9e2dc9d
jviglio <30503240+jviglio@users.noreply.github.com>
2023-06-13 12:33:30 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+var createCounter = function(n) {
+  return function() {
+      return n++;
+  };
+};
+
+
+const counter = createCounter(10)
+console.log(counter()) 
+console.log(counter())
+console.log(counter())
