9edbbe2bf6a93629da9d6f5cecca78e47ba3e4fd
jviglio <30503240+jviglio@users.noreply.github.com>
2023-06-16 09:40:55 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Import stylesheets
 import './style.css';
 
-var createCounter = function(n) {
-  return function() {
-      return n++;
-  };
-};
+// var createCounter = function(n) {
+//   return function() {
+//       return n++;
+//   };
+// };
+
+const createCounter = n => () => n++;
 
 
 const counter = createCounter(10)
